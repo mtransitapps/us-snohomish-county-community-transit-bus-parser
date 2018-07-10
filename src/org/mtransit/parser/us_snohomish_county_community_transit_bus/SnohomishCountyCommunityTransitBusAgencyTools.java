5634772132c07a4c3ebbb403e5b0d820e25da633
Mathieu Méa <mathieu.mea@gmail.com>
2018-07-10 09:42:57 -0400

    Compatible with Current/Next GTFS data

--- src/org/mtransit/parser/us_snohomish_county_community_transit_bus/SnohomishCountyCommunityTransitBusAgencyTools.java
+++ src/org/mtransit/parser/us_snohomish_county_community_transit_bus/SnohomishCountyCommunityTransitBusAgencyTools.java
@@ -54,6 +54,11 @@ public class SnohomishCountyCommunityTransitBusAgencyTools extends DefaultAgency
 	}
 
 	@Override
+	public boolean excludingAll() {
+		return this.serviceIds != null && this.serviceIds.isEmpty();
+	}
+
+	@Override
 	public boolean excludeCalendar(GCalendar gCalendar) {
 		if (this.serviceIds != null) {
 			return excludeUselessCalendar(gCalendar, this.serviceIds);
