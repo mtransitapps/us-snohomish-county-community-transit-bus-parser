4f97991adf7421386b9689ec3ba812316ae1ef9e
Mathieu Méa <mathieu.mea@gmail.com>
2018-07-10 09:46:23 -0400

    Compatible with Current/Next GTFS data

--- parse_next.sh
+++ parse_next.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo ">> Parsing Current...";
+echo ">> Parsing Next...";
 PARSER_DIRECTORY="../parser";
 PARSER_CLASSPATH=$(cat "$PARSER_DIRECTORY/classpath")
 CLASS=$(cat "parser_class")
 echo "CLASS: $CLASS";
 CHANGE_DIRECTORY=$(cat "change_directory");
 echo "CHANGE_DIRECTORY: $CHANGE_DIRECTORY";
-RES_DIR="res-current"
+RES_DIR="res-next"
 echo "RES_DIR: $RES_DIR";
-ARGS="input/gtfs.zip $CHANGE_DIRECTORY$RES_DIR/raw current_"
+ARGS="input/gtfs.zip $CHANGE_DIRECTORY$RES_DIR/raw next_"
 echo "ARGS: $ARGS";
 java -Xms2048m -Xmx8192m -Dfile.encoding=UTF-8 \
 -classpath \
@@ -17,5 +17,5 @@ $PARSER_CLASSPATH \
 $CLASS \
 $ARGS;
 RESULT=$?;
-echo ">> Parsing Current... DONE";
+echo ">> Parsing Next... DONE";
 exit $RESULT;
